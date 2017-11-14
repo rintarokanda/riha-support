fa47ea8c085d4551a29f4bf3d6f7ae3ca17c19a3
uenoryo <r.r.ag0611@outlook.jp>
2017-11-14 19:43:43 +0900

    Change port from 5000 to 80.

--- tutorial/manage.py
+++ tutorial/manage.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from flask.ext.script import Manager
 from flaskr import app, db
 print(app.url_map)
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0', port=80, debug=True)
 
 manager = Manager(app)
 
